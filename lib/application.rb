796611d9352f66db750244ef66f99ead83785873
Saroumoun <bastien.hiel@gmail.com>
2019-01-24 23:03:40 +0100

    removed old code

--- lib/application.rb
+++ lib/application.rb
@@ -7,14 +7,6 @@ class Application
     puts "BIENVENUE! Vous allez jouer au morpion :)"
     @game = Game.new
     @game.turn
-
- #    count_turns = 1
-
-	#     while @game.status == "on going" && count_turns < 10
-	#       puts "Tour #{@count_turns}"
-	#       @game.turn
-	#       count_turns += 1
-	#     end
- #    @game.status = "nul"
   end
+  
 end
